86a6a05ad05fb6f12e8bbbdc2f4ed7b5e126718f
idodoron11 <ido.doron@gmail.com>
2021-07-29 16:36:50 +0300

    Initial commit

---
+++ src/CardGUI.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CardGUI extends JFrame {
+    private JPanel mainPanel;
+    private JPanel Content;
+    private JPanel Dashboard;
+    private JButton wellButton;
+    private JButton vagueButton;
+    private JButton badButton;
+    private JButton exposeAnswerButton;
+    private JLabel questionDescription;
+    private JLabel questionAnswer;
+
+    public CardGUI(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        this.hideAnswer();
+        exposeAnswerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exposeAnswer();
+            }
+        });
+    }
+
+    public void exposeAnswer() {
+        this.exposeAnswerButton.setVisible(false);
+        this.questionAnswer.setVisible(true);
+    }
+
+    public void hideAnswer() {
+        this.exposeAnswerButton.setVisible(true);
+        this.questionAnswer.setVisible(false);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new CardGUI("MemorizeIt");
+        frame.setVisible(true);
+    }
+}
---
+++ src/CardsQueue.java
@@ -0,0 +1,6 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class CardsQueue {
+    Queue<Question> queue = new PriorityQueue<>();
+}

f0ac203b993f32510989cfdd84ce815d20267836
idodoron11 <ido.doron@gmail.com>
2021-08-03 15:26:35 +0300

    Refactoring

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -57,163 +57,6 @@ public class MainGUI extends JFrame {
         });
     }
 
-    private void loadMenuBar() {
-        topMenu = new JMenuBar();
-        this.setJMenuBar(topMenu);
-        setupFileMenu();
-        setupEditMenu();
-        topMenu.add(fileMenu);
-        topMenu.add(editMenu);
-        setupMenuBarActionListeners();
-    }
-
-    private void setupFileMenu() {
-        fileMenu = new JMenu("File");
-        clearInteractionsMenuItem = new JMenuItem("Reset interactions history");
-        fileMenu.add(clearInteractionsMenuItem);
-        deleteAllCardsMenuItem = new JMenuItem("Permanently delete all cards");
-        fileMenu.add(deleteAllCardsMenuItem);
-        exportCardsMenuItem = new JMenuItem("Export cards to csv file.");
-        fileMenu.add(exportCardsMenuItem);
-        importCardsMenuItem = new JMenuItem("Import cards from csv file.");
-        fileMenu.add(importCardsMenuItem);
-        settingsMenuItem = new JMenuItem("Settings");
-        fileMenu.add(settingsMenuItem);
-    }
-
-    private void setupEditMenu() {
-        editMenu = new JMenu("Edit");
-        addNewCardMenuItem = new JMenuItem("Add new card");
-        editMenu.add(addNewCardMenuItem);
-        editCurrentCardMenuItem = new JMenuItem("Edit current card");
-        editMenu.add(editCurrentCardMenuItem);
-        removeCurrentCardMenuItem = new JMenuItem("Remove current card");
-        editMenu.add(removeCurrentCardMenuItem);
-    }
-
-    private void setupMenuBarActionListeners() {
-        // File -> Reset interactions history
-        clearInteractionsMenuItem.addActionListener(e -> {
-            long[] result = new ClearInteractionsDialog(MainGUI.this).showDialog();
-            if (result != null) {
-                if (result[0] == -1) {
-                    // Clear all interactions
-                    this.mgr.resetAllInteractions();
-                } else {
-                    // Clear interactions in range
-                    this.mgr.resetInteractions(result[0], result[1]);
-                }
-                this.mgr.queue.refreshQueue();
-                this.showNextCard();
-            }
-        });
-
-        // File -> Permanently delete all cards
-        deleteAllCardsMenuItem.addActionListener(e -> {
-            int result = JOptionPane.showConfirmDialog(MainGUI.this,
-                    "Are you sure you want to permanently delete all cards?",
-                    "Confirmation Dialog",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE);
-            if (result == JOptionPane.YES_NO_OPTION) {
-                MainGUI.this.mgr.deleteAllCards();
-                MainGUI.this.mgr.queue.refreshQueue();
-                MainGUI.this.showNextCard();
-            }
-        });
-
-        // File -> Export cards to csv.
-        exportCardsMenuItem.addActionListener(e -> {
-            JFileChooser fc = new Utils.SaveDialogWithConfirmation();
-            fc.setFileFilter(Utils.csvFileFilter);
-            int returnVal = fc.showSaveDialog(MainGUI.this);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fc.getSelectedFile();
-                if (!Utils.csvFileFilter.accept(file)) {
-                    file = new File(file.toString() + ".csv");
-                }
-                if (MainGUI.this.mgr.exportCards(file)) {
-                    JOptionPane.showMessageDialog(MainGUI.this,
-                            "The cards were successfully exported to " +
-                                    file.getPath() +
-                                    ".",
-                            "Success",
-                            JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(MainGUI.this,
-                            "An error occur. See log for details.",
-                            "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-
-        // File -> Import cards from csv.
-        importCardsMenuItem.addActionListener(e -> {
-            JFileChooser fc = new JFileChooser();
-            fc.setFileFilter(Utils.csvFileFilter);
-            int returnVal = fc.showDialog(MainGUI.this, "Import");
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fc.getSelectedFile();
-                if (!MainGUI.this.mgr.importCards(file)) {
-                    JOptionPane.showMessageDialog(MainGUI.this,
-                            "An error occur. Please make sure to choose a comma-separated csv " +
-                                    "file with exactly two columns.",
-                            "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(MainGUI.this,
-                            "The cards were successfully imported.",
-                            "Success",
-                            JOptionPane.INFORMATION_MESSAGE);
-                    MainGUI.this.mgr.queue.refreshQueue();
-                    MainGUI.this.showNextCard();
-                }
-            }
-        });
-
-        // File -> Settings
-        settingsMenuItem.addActionListener(e -> {
-            if(new Settings("Settings", MainGUI.this).showDialog()) {
-                // Settings changed, and so we need to refresh the screen.
-                this.mgr.queue.refreshQueue();
-                this.showNextCard();
-            }
-        });
-
-        // Edit -> Add new card.
-        addNewCardMenuItem.addActionListener(e -> {
-            String[] card = new CardEditDialog(MainGUI.this).showDialog();
-            if (card != null) {
-                mgr.insertNewCard(card[0], card[1]);
-                mgr.queue.refreshQueue();
-                this.showNextCard();
-            }
-        });
-
-        // Edit -> Edit current card.
-        editCurrentCardMenuItem.addActionListener(e -> {
-            String[] card = new CardEditDialog(MainGUI.this, this.currentCard).showDialog();
-            if (card != null) {
-                this.currentCard.updateCard(card[0], card[1]);
-                this.showCard(currentCard);
-            }
-        });
-
-        // Edit -> Remove current card.
-        removeCurrentCardMenuItem.addActionListener( e -> {
-            int result = JOptionPane.showConfirmDialog(MainGUI.this,
-                    "Are you sure you want to delete the current card?",
-                    "Confirmation Dialog",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE);
-            if (result == JOptionPane.YES_NO_OPTION) {
-                this.currentCard.deleteCard();
-                this.showNextCard();
-            }
-        });
-    }
-
     private void exposeAnswer() {
         this.exposeAnswerButton.setVisible(false);
         this.answerWrapper.setVisible(true);
@@ -271,4 +114,179 @@ public class MainGUI extends JFrame {
             this.removeCurrentCardMenuItem.setEnabled(true);
         }
     }
+
+    private void loadMenuBar() {
+        topMenu = new JMenuBar();
+        this.setJMenuBar(topMenu);
+        setupFileMenu();
+        setupEditMenu();
+        topMenu.add(fileMenu);
+        topMenu.add(editMenu);
+        setupMenuBarActionListeners();
+    }
+
+    private void setupFileMenu() {
+        fileMenu = new JMenu("File");
+        clearInteractionsMenuItem = new JMenuItem("Reset interactions history");
+        fileMenu.add(clearInteractionsMenuItem);
+        deleteAllCardsMenuItem = new JMenuItem("Permanently delete all cards");
+        fileMenu.add(deleteAllCardsMenuItem);
+        exportCardsMenuItem = new JMenuItem("Export cards to csv file.");
+        fileMenu.add(exportCardsMenuItem);
+        importCardsMenuItem = new JMenuItem("Import cards from csv file.");
+        fileMenu.add(importCardsMenuItem);
+        settingsMenuItem = new JMenuItem("Settings");
+        fileMenu.add(settingsMenuItem);
+    }
+
+    private void setupEditMenu() {
+        editMenu = new JMenu("Edit");
+        addNewCardMenuItem = new JMenuItem("Add new card");
+        editMenu.add(addNewCardMenuItem);
+        editCurrentCardMenuItem = new JMenuItem("Edit current card");
+        editMenu.add(editCurrentCardMenuItem);
+        removeCurrentCardMenuItem = new JMenuItem("Remove current card");
+        editMenu.add(removeCurrentCardMenuItem);
+    }
+
+    private void setupMenuBarActionListeners() {
+        setupClearInteractionsActionListener(clearInteractionsMenuItem);
+        setupDeleteAllCardsActionListener(deleteAllCardsMenuItem);
+        setupExportCardsActionListener(exportCardsMenuItem);
+        setupImportCardsActionListener(importCardsMenuItem);
+        setupSettingsActionListener(settingsMenuItem);
+        setupAddNewCardActionListener(addNewCardMenuItem);
+        setupEditCurrentCardActionListener(editCurrentCardMenuItem);
+        setupRemoveCurrentCardActionListener(removeCurrentCardMenuItem);
+    }
+
+    private void setupClearInteractionsActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            long[] result = new ClearInteractionsDialog(MainGUI.this).showDialog();
+            if (result != null) {
+                if (result[0] == -1) {
+                    // Clear all interactions
+                    this.mgr.resetAllInteractions();
+                } else {
+                    // Clear interactions in range
+                    this.mgr.resetInteractions(result[0], result[1]);
+                }
+                this.mgr.queue.refreshQueue();
+                this.showNextCard();
+            }
+        });
+    }
+
+    private void setupDeleteAllCardsActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(MainGUI.this,
+                    "Are you sure you want to permanently delete all cards?",
+                    "Confirmation Dialog",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+            if (result == JOptionPane.YES_NO_OPTION) {
+                MainGUI.this.mgr.deleteAllCards();
+                MainGUI.this.mgr.queue.refreshQueue();
+                MainGUI.this.showNextCard();
+            }
+        });
+    }
+
+    private void setupExportCardsActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            JFileChooser fc = new Utils.SaveDialogWithConfirmation();
+            fc.setFileFilter(Utils.csvFileFilter);
+            int returnVal = fc.showSaveDialog(MainGUI.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                if (!Utils.csvFileFilter.accept(file)) {
+                    file = new File(file.toString() + ".csv");
+                }
+                if (MainGUI.this.mgr.exportCards(file)) {
+                    JOptionPane.showMessageDialog(MainGUI.this,
+                            "The cards were successfully exported to " +
+                                    file.getPath() +
+                                    ".",
+                            "Success",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(MainGUI.this,
+                            "An error occur. See log for details.",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
+    private void setupImportCardsActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            JFileChooser fc = new JFileChooser();
+            fc.setFileFilter(Utils.csvFileFilter);
+            int returnVal = fc.showDialog(MainGUI.this, "Import");
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                if (!MainGUI.this.mgr.importCards(file)) {
+                    JOptionPane.showMessageDialog(MainGUI.this,
+                            "An error occur. Please make sure to choose a comma-separated csv " +
+                                    "file with exactly two columns.",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(MainGUI.this,
+                            "The cards were successfully imported.",
+                            "Success",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    MainGUI.this.mgr.queue.refreshQueue();
+                    MainGUI.this.showNextCard();
+                }
+            }
+        });
+    }
+
+    private void setupSettingsActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            if(new Settings("Settings", MainGUI.this).showDialog()) {
+                // Settings changed, and so we need to refresh the screen.
+                MainGUI.this.mgr.queue.refreshQueue();
+                MainGUI.this.showNextCard();
+            }
+        });
+    }
+
+    private void setupAddNewCardActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            String[] card = new CardEditDialog(MainGUI.this).showDialog();
+            if (card != null) {
+                mgr.insertNewCard(card[0], card[1]);
+                mgr.queue.refreshQueue();
+                MainGUI.this.showNextCard();
+            }
+        });
+    }
+
+    private void setupEditCurrentCardActionListener(AbstractButton btn) {
+        btn.addActionListener(e -> {
+            String[] card = new CardEditDialog(MainGUI.this, MainGUI.this.currentCard).showDialog();
+            if (card != null) {
+                MainGUI.this.currentCard.updateCard(card[0], card[1]);
+                MainGUI.this.showCard(currentCard);
+            }
+        });
+    }
+
+    private void setupRemoveCurrentCardActionListener(AbstractButton btn) {
+        btn.addActionListener( e -> {
+            int result = JOptionPane.showConfirmDialog(MainGUI.this,
+                    "Are you sure you want to delete the current card?",
+                    "Confirmation Dialog",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+            if (result == JOptionPane.YES_NO_OPTION) {
+                MainGUI.this.currentCard.deleteCard();
+                MainGUI.this.showNextCard();
+            }
+        });
+    }
+
 }
